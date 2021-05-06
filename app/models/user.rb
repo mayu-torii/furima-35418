1101a19575089f4ef55c6fca99d0c82ee84e7c1c
mayu-torii <mayu1152525@yahoo.co.jp>
2021-05-06 18:46:58 +0900

    新規登録、ログイン、ログアウト機能設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :products
+  has_many :purchase_histories
+
+  
+  validates :nickname, presence: true
+  validates :family_name, presence: true
+  validates :first_name, presence: true
+  validates :family_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birthday, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+  validates_format_of :password, with: PASSWORD_REGEX
+
+
 end
