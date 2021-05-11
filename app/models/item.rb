50e8fb148eb63779fed21b852cb400df4c61e042
mayu-torii <mayu1152525@yahoo.co.jp>
2021-05-11 17:08:10 +0900

    プルダウンの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,16 +1,31 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee
+  belongs_to :shipping_place
+  belongs_to :shipping_days
 
+  
   belongs_to :user
   has_one :purchase_history
   has_one_attached :image
 
+
+  validates :image, presence: true
   validates :name, presence: true
   validates :description, presence: true
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :shipping_fee_id, presence: true
-  validates :shipping_place_id, presence: true
-  validates :shipping_days_id, presence: true
-  validates :price, presence: true
 
+  
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_fee_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_place_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_days_id, numericality: { other_than: 1, message: "can't be blank" } 
+
+  validates :price, numericality: { with: /\A[0-9]+\z/, message: "can't be blank" }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'out of setting range' }
+             
+             
 end
+
