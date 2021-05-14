e512d0e5a38c612717c2d2e0680b8d5e14f2f3e9
mayu-torii <mayu1152525@yahoo.co.jp>
2021-05-14 11:19:41 +0900

    private、before_action内記述変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
+  before_action :set_item, except: [:index, :new, :create]
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :contributor_confirmation, only: [:edit, :update]
   
 
   def index
@@ -20,16 +22,13 @@ class ItemsController < ApplicationController
   end  
 
   def show
-    @item = Item.find(params[:id])
   end  
 
   def edit
-    @item = Item.find(params[:id])
-    redirect_to items_path unless @item.user_id == current_user.id   
+    redirect_to items_path unless @item.user_id == current_user.id
   end
 
   def update
-     @item = Item.find(params[:id])
      if @item.update(item_params)
         redirect_to item_path
      else
@@ -43,7 +42,12 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :shipping_fee_id, :shipping_place_id, :shipping_days_id, :price).merge(user_id: current_user.id)
   end
 
+  def set_item
+    @item = Item.find(params[:id])
+  end
   
-  
+  def contributor_confirmation
+    redirect_to items_path unless @item.user_id == current_user.id
+  end
   
 end
