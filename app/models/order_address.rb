f8619452af1f0c9620cc61384b2c8ebe82a8b681
mayu-torii <mayu1152525@yahoo.co.jp>
2021-05-15 13:49:40 +0900

    バリデーション設定

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,10 +1,18 @@
 class OrderAdress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture, :city, :street_number, :building_name, :telephone, :user_id, :item_id
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture, :city, :street_number, :building_name, :telephone
 
-  # ここにバリデーションの処理を書く
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :city
+    validates :street_number
+    validates :telephone, format: { with: /\A[0-9]{11}\z/, message: 'Input only number' }
+  do
+    validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
 
   def save
-    # 各テーブルにデータを保存する処理を書く
+    
   end
 end
