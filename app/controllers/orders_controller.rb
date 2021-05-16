19d372a7453198131e7543bf417123fb78bf0bb8
mayu-torii <mayu1152525@yahoo.co.jp>
2021-05-16 19:53:21 +0900

    フォーム情報の保存設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,35 +1,31 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
   before_action :set_item, only: [:index, :create]
-  before_action :move_to_root_path, only: [:index, :create]
-
 
   def index
     @order_address = OrderAddress.new
   end
 
+
   def create
-    @order_address = OrderAddress.new(order_address_params)
-    if @order_address.save
-       redirect_to root_path
+    @order_address = OrderAddress.new(order_params)
+    @order_address.save
+    if @order_address.valid?
+      redirect_to root_path
     else
-       render :index
-    end   
+      render :index
+    end
   end
 
 
   private
 
-  def order_address_params
-    params.permit(:postal_code, :prefecture_id, :city, :street_number, :building_name, :telephone).merge(user_id: current_user.id, item_id: params[:item_id])
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :street_number, :building_name, :telephone).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
   def set_item
     @item = Item.find(params[:item_id])
-  end
-
-  def move_to_root_path
-    redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
   end
 
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,19 +1,21 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :street_number, :building_name, :telephone
+  attr_accessor :postal_code, :prefecture_id, :city, :street_number, :building_name, :order_id, :telephone, :user_id, :item_id
 
   with_options presence: true do
     validates :user_id
     validates :item_id
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
     validates :city
     validates :street_number
-    validates :telephone, format: { with: /\A[0-9]{11}\z/, message: 'Input only number' }
+    validates :telephone, format: { with: /\A0[0-9]{10,11}\z/, message: 'is invalid. Input only number' }
+    validates :telephone, length: { maximum: 10, message: 'is too short' }
   end
-    validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+    
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
-    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, street_number: street_number, building_name: building_name, telephone: telephone, order_id: order.id)
+    Address.create!(postal_code: postal_code, prefecture_id: prefecture_id, city: city, street_number: street_number, building_name: building_name, telephone: telephone, order_id: order.id)
   end
 end
