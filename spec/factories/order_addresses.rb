d4f24d3a8a41137da6f78bebea6b8247b5e16111
mayu-torii <mayu1152525@yahoo.co.jp>
2021-05-20 20:26:31 +0900

    FactoryBotへ建物名追加

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
     prefecture_id { 2 }
     city { '江東区' }
     street_number { '1-1' }
+    building_name { '建物名' }
     telephone { '09012345678' }
     token {"tok_abcdefghijk00000000000000000"}
   end
