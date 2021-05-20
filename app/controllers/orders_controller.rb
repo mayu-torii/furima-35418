36f784e23b1108f37025324cc26b01af93710aee
mayu-torii <mayu1152525@yahoo.co.jp>
2021-05-20 14:13:28 +0900

    決済機能サーバーサイド実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,8 +9,9 @@ class OrdersController < ApplicationController
 
   def create
     @order_address = OrderAddress.new(order_params)
-    @order_address.save
     if @order_address.valid?
+      pay_item
+      @order_address.save
       redirect_to root_path
     else
       render :index
@@ -21,11 +22,20 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :street_number, :building_name, :telephone).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :street_number, :building_name, :telephone).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   def set_item
     @item = Item.find(params[:item_id])
   end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],     
+      currency: 'jpy'                 
+    )
+  end  
 
 end
