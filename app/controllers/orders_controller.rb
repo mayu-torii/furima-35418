1c95db497fa6a2d8532e2f7256b3b5979e5041ff
mayu-torii <mayu1152525@yahoo.co.jp>
2021-05-20 17:00:46 +0900

    状況に応じてトップページに遷移

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
   before_action :set_item, only: [:index, :create]
+  before_action :move_to_root_path, only: [:index, :create]
 
   def index
     @order_address = OrderAddress.new
@@ -9,7 +10,6 @@ class OrdersController < ApplicationController
 
   def create
     @order_address = OrderAddress.new(order_params)
-
     if @order_address.valid?
       pay_item
       @order_address.save
@@ -29,6 +29,10 @@ class OrdersController < ApplicationController
   def set_item
     @item = Item.find(params[:item_id])
   end
+
+  def move_to_root_path
+    redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
+  end  
 
   def pay_item
     Payjp.api_key = "sk_test_6f04409bef14067298c27717"
